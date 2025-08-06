2db7b824bb903e15047da1d2a0aa20a52083e0b4
Juliana Neves <nevesajuliana@gmail.com>
2025-08-06 13:07:33 +0100

    Add real-time date and time display for multiple time zones

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('Hello');
+let losAngelesEl = document.querySelector("#los-angeles");
+let losAngelesDateEl = losAngelesEl.querySelector(".date");
+let losAngelesTimeEl = losAngelesEl.querySelector(".time");
+let losAngelesTZ = "America/Los_Angeles";
+
+let londonEl = document.querySelector("#london");
+let londonDateEl = londonEl.querySelector(".date");
+let londonTimeEl = londonEl.querySelector(".time");
+let londonTZ = "Europe/London";
+
+let sydneyEl = document.querySelector("#sydney");
+let sydneyDateEl = sydneyEl.querySelector(".date");
+let sydneyTimeEl = sydneyEl.querySelector(".time");
+let sydneyTZ = "Australia/Sydney";
+
+let tokyoEl = document.querySelector("#tokyo");
+let tokyoDateEl = tokyoEl.querySelector(".date");
+let tokyoTimeEl = tokyoEl.querySelector(".time");
+let tokyoTZ = "Asia/Tokyo";
+
+function updateTime(countryDate, countryTime, countryTZ) {
+  let now = moment();
+  countryDate.innerHTML = now.format("ddd, MMMM Mo, YYYY");
+  countryTime.innerHTML = now
+    .tz(countryTZ)
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+
+function init() {
+  updateTime(losAngelesDateEl, losAngelesTimeEl, losAngelesTZ);
+  updateTime(londonDateEl, londonTimeEl, londonTZ);
+  updateTime(sydneyDateEl, sydneyTimeEl, sydneyTZ);
+  updateTime(tokyoDateEl, tokyoTimeEl, tokyoTZ);
+}
+
+init();
+setInterval(init, 1000);
